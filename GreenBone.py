10644aeed7114ec2721fca3b15ef6fd4cdf26841
LongJeffreySilver <jeffreythedestroyer@gmail.com>
2022-06-08 15:24:01 -0400

    Ya funciona el lanzamiento de la task y la descarga del reporte en formato CSV

--- GreenBone.py
+++ GreenBone.py
@@ -5,51 +5,40 @@ class GreenBone:
     #https://python-gvm.readthedocs.io/en/latest/api/gmpv214.html#gvm.protocols.gmpv214.Gmp.send_command 
     #Se pueden hacer uso de scripts previamente creados en python https://github.com/greenbone/gvm-tools/tree/main/scripts
     #Llamar con subprocess a un gvm-script con el script que usa el gmp -> guiarme con https://github.com/greenbone/gvm-tools/blob/main/scripts/create-targets-from-host-list.gmp.py
-    
-    '''gvm-pyshell \
-    --gmp-username webadmin --gmp-password kennwort'''
-    
-    def borrarTargets(idTarget):
-        # delete_target(target_id, *, ultimate=True) #Ultimate es para eliminar por completo incluso del cubo de basura
-        a
+    '''
+    FIXME
+
+    Sacar de alguna forma la contraseña y el usuario de Greenbone
+    Hacer una carpeta para los scripts y cambiar la ruta en los comandos
+    Pasarle la ruta de donde se va a guardar el reporte
+    '''
+
 
     def crearTargets(rutaFichero):
-        proceso = subprocess.run(["gvm-script", "--gmp-username", "admin", "--gmp-password", "8e3898cc-8bce-4506-898f-e5904b317c55", "socket", "/home/kali/Desktop/TFG-1/create-targets-from-host-list.gmp.py", rutaFichero],capture_output=True,text=True) #rutaFichero"/home/kali/Desktop/TFG-1/Ficheros_de_Salida/conjunto_IP.txt"       
+        proceso = subprocess.run(["gvm-script", "--gmp-username", "admin", "--gmp-password", "8e3898cc-8bce-4506-898f-e5904b317c55", "socket",
+        "/home/kali/Desktop/TFG-1/create-targets-from-host-list.gmp.py", rutaFichero],capture_output=True,text=True) #rutaFichero"/home/kali/Desktop/TFG-1/Ficheros_de_Salida/conjunto_IP.txt"       
         idTarget = proceso.stdout.splitlines()        
         return idTarget #Devuelve el id del target que tiene todas las IPs dentro  
     
     def crearTask(idTarget):
-
-        '''
-        Segun la documentacion hay que sacar el id de la configuracion
-        res = gmp.get_configs()
-        >>> for i, conf in enumerate(res.xpath('config')):
-        configuracion_id = conf.xpath('@id')[0]
-        name = conf.xpath('name/text()')[0]
-        print('\n({0}) {1}: ({2})'.format(i, name, id))
-        
-        Con esto se sacaria el id de la configuracion
-
-        Como solo se va a utilizar por el momento el escaner basico de OpenVas, nos podemos aprovechar de que su id es fijo
-        OpenVAS scanner: 08b69003-5fc2-4037-a479-93b440211c73
-
-
-        Por ultimo se puede crear la task
-
-        res=gmp.create_task(name="Scan Suspect Host",config_id=daba56c8-73ec-11df-a475-002264764cea ,scanner_id="08b69003-5fc2-4037-a479-93b440211c73", target_id=idTargets)
-        >>> idTask = res.xpath('@id')[0]
-
-
-        '''
 
         proceso = subprocess.run(["gvm-script", "--gmp-username", "admin", "--gmp-password", "8e3898cc-8bce-4506-898f-e5904b317c55", "socket", 
         "/home/kali/Desktop/TFG-1/create_task.py", idTarget],capture_output=True,text=True)
         idTask = proceso.stdout.splitlines()
         return idTask
 
-    def lanzarTask(idTask):
-        #gmp.start_task(idTask)
-        a
+    def lanzarTask(idTask): #FIXME Tengo que ver si suelta algun error para capturarlo
+        proceso = subprocess.Popen(["gvm-script", "--gmp-username", "admin", "--gmp-password", "8e3898cc-8bce-4506-898f-e5904b317c55", "socket", 
+        "/home/kali/Desktop/TFG-1/start_task.py", idTask])
+    
     def descargarReporte():
-        #hay una funcion get_report
+        #Sacar primero el id del unico reporte que este disponible (el actual)
+        idReport = ""
+        subprocess.Popen(["gvm-script", "--gmp-username", "admin", "--gmp-password", "8e3898cc-8bce-4506-898f-e5904b317c55", "socket", 
+        "/home/kali/Desktop/TFG-1/get_report_csv.py", idReport, "Reporte_greenbone.csv"])
+        #borrar reporte con el id del reporte
+    
+    
+    def borrarTargets(idTarget):
+        # delete_target(target_id, *, ultimate=True) #Ultimate es para eliminar por completo incluso del cubo de basura
         a
---
+++ start_task.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2017-2021 Greenbone Networks GmbH
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+import time
+from argparse import Namespace
+from gvm.protocols.gmp import Gmp
+
+from gvmtools.helper import error_and_exit
+
+
+def check_args(args):
+    len_args = len(args.script) - 1
+    if len_args != 1:
+        message = """
+        Launching the task according to the ID passed as a parameter \
+
+        One parameter after the script name is required.
+
+        1. <task_id>        -- ID of the task (String)
+
+        Example: gvm-script --gmp-username <user> --gmp-password <pass> socket /home/kali/Desktop/start_task.py <task_id>
+
+        """
+        print(message)
+        sys.exit()
+
+def send_command(gmp, idTask):    
+    res = gmp.start_task(idTask)
+    #idReport = res.xpath('@id')[0] #Con esto saco el id del reporte para guardarlo despues
+    #print(idReport)
+
+def main(gmp: Gmp, args: Namespace) -> None:
+    # pylint: disable=undefined-variable
+
+    check_args(args)
+
+    idTask = args.script[1]
+
+    send_command(gmp, idTask)
+
+
+
+
+if __name__ == '__gmp__':
+    main(gmp, args)

448991a7b7e6264cb68c462a36f6a1809be36e00
LongJeffreySilver <87135689+LongJeffreySilver@users.noreply.github.com>
2022-10-03 18:00:52 +0200

    Actualización de los analisis inalambricos y cableados
    Actualizada la forma de comprobar las interfaces y eliminación de Kismet por Tcpdump directamente.

--- Controlador_Herramientas.py
+++ Controlador_Herramientas.py
@@ -2,7 +2,7 @@ import subprocess
 import time
 
 from GreenBone import GreenBone
-
+from threading import Timer
 
 class Controlador_Herramientas:
 
@@ -10,15 +10,12 @@ class Controlador_Herramientas:
 
     def analisisCableado(self,rutaFicherosEntrada): #sudo ettercap -Tqz -s 's(30)lqq' -i eth1 > /home/kali/Desktop/lista.txt 
         
-        #Confirmar si la interfaz cableada eth0 por defecto esta activa con: ip link | grep eth0 -c
-        procesoIP = subprocess.Popen(["ip" ,"link"], stdout=subprocess.PIPE)
-        procesoGrep = subprocess.Popen(('grep', "eth0", "-c"), stdin=procesoIP.stdout, stdout=subprocess.PIPE,text=True)
-        procesoIP.wait()
-        salida = procesoGrep.communicate()
-        #salida = salida[0].strip()
-        hayInterfaz = salida[0].split(sep="\n")
-        #hayInterfaz = aux[0]
-        if hayInterfaz[0] == "1":
+        #Si la conexion a eth0 es up, realiza el analisis. En caso de no existir o ser down, entonces no se realiza el analisis.
+        proceso = subprocess.Popen(["cat", "/sys/class/net/eth0/operstate"], stdout=subprocess.PIPE, stderr=subprocess.PIPE,text=True)
+        salida,err = proceso.communicate()
+
+        estadoInterfaz = salida.split(sep="\n")
+        if estadoInterfaz[0] == "up":
             rutaFichero = rutaFicherosEntrada + "/Entrada_ettercap.txt"
             fichero = open(rutaFichero,"w")
             proceso = subprocess.Popen(["sudo","ettercap", "-Tqz", "-s", "'s(300)lqq'", "-i", "eth0"], stdout=fichero) #Se registran datos durante 5 minutos (300 segundos)
@@ -29,36 +26,47 @@ class Controlador_Herramientas:
             return "-1" #codigo error porque no hay interfaz cableada
 
     def analisisInalambrico(self,rutaFicherosEntrada):
-        #Confirmar si la interfaz inalambrica eth0 por defecto esta activa con: ip link | grep wlan0 -c
-        procesoIP = subprocess.Popen(["ip" ,"link"], stdout=subprocess.PIPE)
-        procesoGrep = subprocess.Popen(('grep', "wlan0", "-c"), stdin=procesoIP.stdout, stdout=subprocess.PIPE,text=True)
-        procesoIP.wait()
-        salida = procesoGrep.communicate()
-        #salida = salida[0].strip()
-        hayInterfaz = salida[0].split(sep="\n")
-        if hayInterfaz[0] == "1":
+        #Si la conexion a wlan0 es up, realiza el analisis. En caso de no existir o ser down, entonces no se realiza el analisis.
+        proceso = subprocess.Popen(["cat", "/sys/class/net/wlan0/operstate"], stdout=subprocess.PIPE, stderr=subprocess.PIPE,text=True)
+        salida,err = proceso.communicate()
 
-            #Ejecutar Kismet
-            #Generar la traza para pasarsela a TCPdump
-            traza = rutaFicherosEntrada + "/Traza_kismet.pcap"
-            #kismet wlan0 generar_traza traza
-            fichero = open(traza,"w")
-            proceso = subprocess.Popen(["kismet", "wlan0", "comando para guardar traza", traza]) #NO hacerlo con sudo
-            proceso.wait()
-            fichero.close()
-            return self.lanzarTcpdump(rutaFicherosEntrada,traza) #Pasarle la ruta de la traza
+        estadoInterfaz = salida.split(sep="\n")
+        if estadoInterfaz[0] == "up":
+            return self.lanzarTcpdump(rutaFicherosEntrada) #Pasarle la ruta de la traza
         else:
             return "-1" #codigo error porque no hay interfaz inalambrica
 
-    def lanzarTcpdump(self,rutaFicherosEntrada,traza): # No se usa la opcion tcp en el comando porque solo coge IPv4
-        #tcpdump -qns 0 -e -r traza 
-        #Genera un fichero para la salida de Kismet + Tcpdump
-        rutaFichero = rutaFicherosEntrada + "/Entrada_tcpdump.txt"
-        fichero = open(rutaFichero,"w")
-        proceso = subprocess.Popen(["tcpdump", "-qns", "0", "e" , "-r", traza], stdout=fichero)
-        fichero.close()
-        return rutaFichero
+    def lanzarTcpdump(self,rutaFicherosEntrada):
+        #Crea y rellena la traza de red inalambrica con tcpdump hasta que pasan 30 segundos.
+        traza = rutaFicherosEntrada+ "/Traza_tcpdump.pcap"
+        fichero = open(traza, "w")
+        def kill(process): return process.kill()
 
+        proceso = subprocess.Popen(["sudo", "tcpdump", "-w", traza, "-i", "wlan0"])
+
+        my_timer = Timer(300, kill, [proceso])
+
+        try:
+            my_timer.start()
+            proceso.communicate()
+        finally:
+            my_timer.cancel()
+            fichero.close()
+
+        #Debido a que la traza se crea con el usuario tcpdump, se cambia al actual para poder tratarla.
+        proceso = subprocess.run(["whoami"], capture_output=True, text=True)
+        whoami = proceso.stdout.splitlines()
+
+        proceso = subprocess.Popen(["sudo", "chown", whoami[0], traza])
+
+        #Se genera un fichero txt con las trazas unicamente de los protocolos upd y tcp, tanto con IPv4 como IPv6.
+        rutaFichero = rutaFicherosEntrada + "/Entrada_tcpdump.txt"
+        fichero = open(rutaFichero, "w")
+        proceso = subprocess.Popen(
+            ["tcpdump", "udp", "or", "tcp", "-qns", "0", "-e", "-t", "-r", traza], stdout=fichero)
+        fichero.close()
+
+        return rutaFichero
     def escanearRed(self,rutaFicherosEntrada): #Ambos generan unos ficheros que se usan mas adelante
         ficheroCableado = self.analisisCableado(rutaFicherosEntrada=rutaFicherosEntrada)
         ficheroInalambrico = self.analisisInalambrico(rutaFicherosEntrada=rutaFicherosEntrada)
